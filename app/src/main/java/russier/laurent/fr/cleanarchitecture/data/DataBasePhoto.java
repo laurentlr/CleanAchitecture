82914cb81e9762fcb0855b8c7d9efb327388279a
laurent russier <lrussier@xebia.fr>
2017-01-24 21:27:49 +0100

    Create DataBasePhoto

---
+++ app/src/main/java/russier/laurent/fr/cleanarchitecture/data/DataBasePhoto.java
@@ -0,0 +1,50 @@
+package russier.laurent.fr.cleanarchitecture.data;
+
+import com.orm.SugarRecord;
+
+public class DataBasePhoto extends SugarRecord {
+
+    private int photoId;
+    private int albumId;
+    private String title;
+    private String thumbnailUrl;
+
+    public DataBasePhoto(int photoId, int albumId, String title, String thumbnailUrl) {
+        this.photoId = photoId;
+        this.albumId = albumId;
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public int getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(int photoId) {
+        this.photoId = photoId;
+    }
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
